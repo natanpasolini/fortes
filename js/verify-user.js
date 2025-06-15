839f1309a0ef09f637e35903379d7c7f593dbfd9
Natan Pasolini <natanpasoliniob@gmail.com>
2025-06-15 17:35:15 -0300

    update final

--- js/verify-user.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let paginaAtual = window.location.pathname.split('/').pop().replace('.html', '').split('-')[0];
-        const tipoUsuario = localStorage.getItem('tipoUsuarioLogado');
-        if (tipoUsuario === 'ong' && !window.location.pathname.includes(`ong`)) {
-            window.location.href = `./${paginaAtual}-ong.html`;
-        } else if (tipoUsuario === 'coletor' && !window.location.pathname.includes(`coletor`)) {
-            window.location.href = `./${paginaAtual}-coletor.html`;
-        }
-});
